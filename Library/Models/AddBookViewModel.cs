c4bf8ab70837efd2a07a4d88767105fbca12f454
Valentin <85406438+valkata033@users.noreply.github.com>
2022-10-22 15:53:25 +0300

    Add files via upload

---
+++ Library/Models/AddBookViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Library.Data.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class AddBookViewModel
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 10)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string Author { get; set; } = null!;
+
+        [Required]
+        [StringLength(5000, MinimumLength = 5)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [Range(typeof(decimal), "0.0", "10.0", ConvertValueInInvariantCulture = true)]
+        public decimal Rating { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; } = new List<Category>();
+
+    }
+}
