6e563636b04f205cab9057831e5220b5aafcece6
Valentin <85406438+valkata033@users.noreply.github.com>
2022-10-22 15:53:58 +0300

    Delete Library directory

--- Library/Data/Entities/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Library.Data.Entities
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public List<ApplicationUserBook> ApplicationUsersBooks { get; set; } = new List<ApplicationUserBook>();
-    }
-}
